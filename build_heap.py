66703206d0501453275762d6592cf897bf56bc8d
anitasulca <125455508+anitasulca@users.noreply.github.com>
2023-03-18 14:49:38 +0000

    TESTS

--- build_heap.py
+++ build_heap.py
@@ -3,28 +3,29 @@
 def build_heap(data):
     swaps = []
     n = len(data)
-    for i in range(n//2, -1, -1):
+    # Start from the last non-leaf node and go backwards
+    for i in range(n // 2, -1, -1):
         sift_down(i, data, swaps)
     return swaps
 
 def sift_down(i, data, swaps):
-    n = len(data)
-    min_idx = i
-    l = 2*i+1
-    if l < n and data[l] < data[min_idx]:
-        min_idx = l
-    r = 2*i+2
-    if r < n and data[r] < data[min_idx]:
-        min_idx = r
-    if i != min_idx:
-        data[i], data[min_idx] = data[min_idx], data[i]
-        swaps.append((i, min_idx))
-        sift_down(min_idx, data, swaps)
+    # Find the index of the minimum child
+    min_index = i
+    left_child = 2 * i + 1
+    if left_child < len(data) and data[left_child] < data[min_index]:
+        min_index = left_child
+    right_child = 2 * i + 2
+    if right_child < len(data) and data[right_child] < data[min_index]:
+        min_index = right_child
+    # If the minimum child is different from i, swap and sift down further
+    if i != min_index:
+        swaps.append((i, min_index))
+        data[i], data[min_index] = data[min_index], data[i]
+        sift_down(min_index, data, swaps)
 
 def main():
     n = int(input())
     data = list(map(int, input().split()))
-
     assert len(data) == n
 
     swaps = build_heap(data)
