940f84ab520728501490ad91b432129b800a9a09
anitasulca <125455508+anitasulca@users.noreply.github.com>
2023-03-18 16:29:11 +0000

    tests

--- build_heap.py
+++ build_heap.py
@@ -1,67 +1,53 @@
 # python3
 
-def sift_down(arr, i, n, swaps):
-    while 2*i + 1 < n:
-        j = i  # parent
-        l = 2*i + 1  # left child
-        r = 2*i + 2  # right child
-        
-        if arr[l] < arr[j]:
-            j = l
-        if r < n and arr[r] < arr[j]:
-            j = r
-        
-        if i != j:
-            arr[i], arr[j] = arr[j], arr[i]
-            swaps.append((i, j))
-            i = j
-        else:
-            break
+def small(data, i, swaps):
+    n = len(data)
+    l = 2*i + 1
+    r = 2*i + 2
+    sm = i
+    if l < n and data[l] < data[sm]:
+        sm = l
+    if r < n and data[r] < data[sm]:
+        sm = r
+    if sm != i:
+        swaps.append((i, sm))
+        data[i], data[sm] = data[sm], data[i]
+        small(data, sm, swaps)
 
-
-def build_heap(arr):
-    n = len(arr)
+def build_heap(data):
     swaps = []
-    
+    n = len(data)
     for i in range(n//2, -1, -1):
-        sift_down(arr, i, n, swaps)
-        
+        small(data, i, swaps)
     return swaps
 
-
 def main():
-    input_type = input("Enter I for keyboard input or F for file input: ").strip()
-
-    if input_type.lower() == 'i':
-        n = int(input())
-        arr = list(map(int, input().split()))
-
-    elif input_type.lower() == 'f':
-        file_name = input("Enter file name: ").strip()
+    evade = input("Enter 'I' for input from keyboard or 'F' for input from file: ").strip().lower()
+    if evade == "f":
+        file = input("Enter file name: ").strip()
         try:
-                file_name = open("./tests/" + file_name, mode = "r")
-                n = int(file_name.readline().strip())
-                arr = list(map(int, file_name.readline().split()))
-
+            with open(file, "r") as f:
+                n = int(f.readline().strip())
+                data = list(map(int, f.readline().split()))
+                assert len(data) == n
+                swaps = build_heap(data)
+                print(len(swaps))
+                for i, j in swaps:
+                    print(i, j)
         except OSError as e:
-            print(f"Error: {e}")
-            return
-        
+            print(e)
+    elif evade == "i":
+        n = int(input("Enter the length of the list: "))
+        data = list(map(int, input("Enter the elements of the list separated by space: ").split()))
+        assert len(data) == n
+        swaps = build_heap(data)
+        print(len(swaps))
+        for i, j in swaps:
+            print(i, j)
     else:
-        print("Invalid input type")
-        return
-
-    assert len(arr) == n
-    assert len(arr) == len(set(arr))
-
-    swaps = build_heap(arr)
-
-    print(len(swaps))
-    assert len(swaps) <= 4 * n
-
-    for i, j in swaps:
-        print(i, j)
-
+        print("Invalid input. Please enter 'I' or 'F'.")
 
 if __name__ == "__main__":
     main()
+
+
