d319b0193cade5650bb214b189da95a98c7d2531
anitasulca <125455508+anitasulca@users.noreply.github.com>
2023-03-18 14:37:17 +0000

    tests

--- build_heap.py
+++ build_heap.py
@@ -1,42 +1,31 @@
 # python3
 
-
-def heapify(data, n, i, swaps):
-    """
-    A function to heapify a subtree rooted with node i
-    """
-    smallest = i  # Initialize smallest as root
-    l = 2 * i + 1  # left = 2*i + 1
-    r = 2 * i + 2  # right = 2*i + 2
-
-    # See if left child of root exists and is smaller than root
-    if l < n and data[l] < data[smallest]:
-        smallest = l
-
-    # See if right child of root exists and is smaller than root
-    if r < n and data[r] < data[smallest]:
-        smallest = r
-
-    # Change root, if needed
-    if smallest != i:
-        swaps.append((i, smallest))
-        data[i], data[smallest] = data[smallest], data[i]  # swap
-
-        # Heapify the root.
-        heapify(data, n, smallest, swaps)
-
 def build_heap(data):
     swaps = []
     n = len(data)
-    # Build a heap from the last non-leaf node by performing heapify
-    for i in range(n // 2 - 1, -1, -1):
-        heapify(data, n, i, swaps)
+    for i in range(n//2, -1, -1):
+        sift_down(i, data, swaps)
     return swaps
 
+def sift_down(i, data, swaps):
+    n = len(data)
+    min_idx = i
+    l = 2*i+1
+    if l < n and data[l] < data[min_idx]:
+        min_idx = l
+    r = 2*i+2
+    if r < n and data[r] < data[min_idx]:
+        min_idx = r
+    if i != min_idx:
+        data[i], data[min_idx] = data[min_idx], data[i]
+        swaps.append((i, min_idx))
+        sift_down(min_idx, data, swaps)
+
 def main():
-    # Input from keyboard
+    # input from keyboard
     n = int(input())
     data = list(map(int, input().split()))
+
     # checks if length of data is the same as the said length
     assert len(data) == n
 
@@ -44,9 +33,12 @@ def main():
     # and give back all swaps
     swaps = build_heap(data)
 
-    # Output how many swaps were made
+    # output how many swaps were made, 
+    # this number should be less than 4n (less than 4*len(data))
+    assert len(swaps) <= 4*n
+
+    # output all swaps
     print(len(swaps))
-    # Output all swaps
     for i, j in swaps:
         print(i, j)
 
